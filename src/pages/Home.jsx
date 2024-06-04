a604ae9052f338247eb6ab87a8e9429451366dd2
Thisaru Supun <thisarusupun1314@gmail.com>
2024-06-05 02:13:50 +0530

    background changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,46 @@
-import React from 'react'
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
-import Logo from '../assets/pandacoding.png'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Logo from "../assets/pandacoding.png";
+import { VortexBackground } from "../components/ui/VortexBackground";
 
 function Home() {
-    return (
-        <div
-            name='home'
-            className='bg-gradient-to-b from-gray-900 to-black w-full h-auto sm:h-screen pt-[60px] pb-[10px]'>
+  return (
+    <VortexBackground>
+      <div
+        name="home"
+        className="w-full h-auto sm:h-screen pt-[60px] pb-[10px]"
+      >
+        {/* container */}
+        <div className="max-w-screen-lg mx-auto px-4 flex flex-col items-center justify-center h-full md:flex-row">
+          <div className="flex flex-col justify-center h-full px-4">
+            <p className="text-yellow-400 text-xl">Hi, My name is</p>
+            <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+              Thisaru Supun
+            </h1>
+            <h2 className="text-3xl sm:text-5xl text-[#8892b0]">
+              I'm a Full Stack Developer
+            </h2>
 
-            {/* container */}
-            <div className='max-w-screen-lg mx-auto px-4 flex flex-col items-center justify-center h-full md:flex-row'>
-
-                <div className='flex flex-col justify-center h-full px-4'>
-                    <p className='text-yellow-400'>Hi, My name is</p>
-                    <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Thisaru Supun</h1>
-                    <h2 className='text-3xl sm:text-5xl text-[#8892b0]'>I'm a Full Stack Developer</h2>
-
-                    <div>
-                        <button className='text-white group bg-gradient-to-b from-gray-900 to-black rounded-xl px-6 py-3 my-2 flex items-center shadow-sm hover:shadow-blue-600'>
-                            View Work
-                            <span className='group-hover:rotate-90 duration-500'>
-                                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-                            </span>
-                        </button>
-                    </div>
-                </div>
-
-                <div>
-                    <img src={Logo} alt="" className='rounded-full mx-auto w-[200px] md:w-[300px] p-4' />
-                </div>
-
+            <div>
+              <button className="text-white group bg-gradient-to-b from-gray-900 to-black rounded-xl px-6 py-3 my-2 flex items-center shadow-sm hover:shadow-blue-600">
+                View Work
+                <span className="group-hover:rotate-90 duration-500">
+                  <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                </span>
+              </button>
             </div>
+          </div>
 
+          <div>
+            <img
+              src={Logo}
+              alt=""
+              className="rounded-full mx-auto w-[200px] md:w-[300px] p-4"
+            />
+          </div>
         </div>
-    )
+      </div>
+    </VortexBackground>
+  );
 }
 
-export default Home
-
-{/* <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-yellow-400'>Hi, My name is</p>
-                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Thisaru Supun</h1>
-                <h2 className='text-3xl sm:text-5xl text-[#8892b0]'>I'm a Full Stack Developer</h2>
-
-                <div>
-                    <button className='text-white group bg-gradient-to-b from-gray-900 to-black rounded-xl px-6 py-3 my-2 flex items-center shadow-sm hover:shadow-blue-600'>
-                        View Work
-                        <span className='group-hover:rotate-90 duration-500'>
-                            <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-                        </span>
-                    </button>
-                </div>
-            </div>
-
-            <img src={Logo} alt="" className='hidden md:flex w-[300px] mx-[900px] my-[-450px] rounded-full' /> */}
+export default Home;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,127 @@
-import React from 'react'
+import { useContext } from "react";
+import currencyconverter from "../assets/projects/currency-converter.png";
+import moviereviews from "../assets/projects/moviereviews.png";
+import employeems from "../assets/projects/Employee-MS.png";
+import contacts from "../assets/projects/contact.jpeg";
+import chatapp from "../assets/projects/chatapp.png";
+import weatherapplication from "../assets/projects/weather-application.png";
+import { GoCodeReview } from "react-icons/go";
+import { GoRocket } from "react-icons/go";
+import ThemeContext from "../context/ThemeContext";
 
 function Projects() {
+  const { theme } = useContext(ThemeContext);
+
+  const projects = [
+    {
+      name: "Chat Application",
+      src: chatapp,
+      demo: "https://realtime-chat-application-ejtf.onrender.com",
+      code: "https://github.com/thisarusupun/realtime-chat-application",
+    },
+    {
+      name: "Contacts Application",
+      src: contacts,
+      demo: "https://contacts-application-client.vercel.app/",
+      code: "https://github.com/thisarusupun/contacts-application.git",
+    },
+    {
+      name: "Currency Converter",
+      src: currencyconverter,
+      demo: "https://thisarusupun-wdp-currencyconveter.netlify.app",
+      code: "https://github.com/thisarusupun/Currency-Converter.git",
+    },
+
+    {
+      name: "Movie Reviews",
+      src: moviereviews,
+      demo: "",
+      code: "https://github.com/thisarusupun/Movie-Reviews.git",
+    },
+    {
+      name: "Weather Application",
+      src: weatherapplication,
+      demo: "https://effortless-mooncake-ecbf3a.netlify.app",
+      code: "https://github.com/thisarusupun/Weather-App.git",
+    },
+    {
+      name: "Employee Management System",
+      src: employeems,
+      demo: "",
+      code: "https://github.com/thisarusupun/Employee-Management-System.git",
+    },
+  ];
+
+  const handleDemo = ({ demo }) => {
+    // window.location.href = demo
+    window.open(demo, "");
+  };
+
+  const handleCode = ({ code }) => {
+    // window.location.href = code
+    window.open(code, "");
+  };
+
   return (
-    <div name='projects'
-      className='bg-gradient-to-b from-gray-800 to-black w-full text-gray-300 h-auto sm:h-screen pt-[60px] pb-[10px]'
+    <div
+      name="projects"
+      className={`${
+        theme
+          ? "bg-gradient-to-b from-gray-900 to-black text-white"
+          : "bg-gray-200 text-black"
+      }
+            w-full h-auto sm:min-h-screen sm:h-auto pt-[60px] pb-[10px] px-[10px] sm:px-[50px] md:px-[100px] sm:pb-[50px]`}
     >
-      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
-        Projects
+      <div className="max-w-screen-lg mx-auto flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Projects
+          </p>
+          <p className="text-2xl pt-6">These are the projects I have done</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map((project, index) => {
+            const { name, src, demo, code } = project;
+            return (
+              <div
+                key={index}
+                className="shadow-md shadow-gray-600 hover:shadow-yellow-400 rounded-lg p-3 flex sm:flex-col"
+              >
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105 w-[150px] h-[100px] sm:h-[200px] sm:w-full"
+                />
+
+                <div className="flex-col items-center justify-center py-2">
+                  <p className="text-center hover:text-yellow-400 text-md sm:text-lg">
+                    {name}
+                  </p>
+
+                  <div className="w-full flex-col sm:flex sm:flex-row items-center justify-center py-2">
+                    <button
+                      className="w-28 px-6 mx-2 py-1 flex duration-200 hover:scale-105 text-white bg-gradient-to-b from-gray-900 to-black rounded-full items-center shadow-sm shadow-gray-400 hover:shadow-blue-600"
+                      onClick={() => handleDemo({ demo })}
+                    >
+                      Demo <GoRocket className="pl-1" size={20} />
+                    </button>
+
+                    <button
+                      className="w-28 px-6 mx-2 mt-2 sm:my-0 py-1 sm:py-1 flex duration-200 hover:scale-105 text-white bg-gradient-to-b from-gray-900 to-black rounded-full items-center shadow-sm shadow-gray-400 hover:shadow-blue-600"
+                      onClick={() => handleCode({ code })}
+                    >
+                      Code <GoCodeReview className="pl-1" size={20} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
